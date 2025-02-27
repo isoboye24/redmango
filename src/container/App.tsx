8940b363f9be69621ea8b7458b19aee3a65ff78e
isoboye24 <isobarguy4real@gmail.com>
2025-02-28 00:30:01 +0100

    I fetched menuItem API and Interface in TS

--- src/container/App.tsx
+++ src/container/App.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import { Header, Footer } from '../components/layout';
+import { menuItemModel } from '../Interfaces';
 
 function App() {
+  const [menuItems, setMenuItems] = useState<menuItemModel[]>([]);
+
+  useEffect(() => {
+    fetch('https://localhost:7250/api/MenuItem')
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setMenuItems(data.result);
+      });
+  }, []);
+
   return (
     <div>
       <Header />
