081045d068bbd9c1a627a2e2d6f35235be73b909
isoboye24 <isobarguy4real@gmail.com>
2025-03-06 17:27:20 +0100

    I added authentication slide and interface

--- src/Storage/Redux/store.ts
+++ src/Storage/Redux/store.ts
@@ -1,18 +1,22 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { menuItemReducer } from './menuItemSlice';
-import { menuItemApi, shoppingCartApi } from '../../APIs';
+import { menuItemApi, shoppingCartApi, AuthApi } from '../../APIs';
 import { shoppingCartReducer } from './shoppingCartSlice';
+import { userAuthReducer } from './userAuthSlice';
 
 const store = configureStore({
   reducer: {
     menuItemStore: menuItemReducer,
     shoppingCartStore: shoppingCartReducer,
+    userAuthStore: userAuthReducer,
     [menuItemApi.reducerPath]: menuItemApi.reducer,
     [shoppingCartApi.reducerPath]: shoppingCartApi.reducer,
+    [AuthApi.reducerPath]: AuthApi.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware()
       .concat(menuItemApi.middleware)
+      .concat(AuthApi.middleware)
       .concat(shoppingCartApi.middleware),
 });
 
