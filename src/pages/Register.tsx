c48b197159f59323ccc0952e2fa2bd7c7af85500
isoboye24 <isobarguy4real@gmail.com>
2025-03-06 18:22:03 +0100

    I implemented register user in action

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 import { SD_Roles } from '../Utility/SD';
 import { inputHelper } from '../helper';
+import { useRegisterUserMutation } from '../APIs/AuthApi';
+import { apiResponse } from '../Interfaces';
 
 const Register = () => {
+  const [registerUser] = useRegisterUserMutation();
   const [loading, setLoading] = useState(false);
   const [userInput, setUserInput] = useState({
     userName: '',
@@ -18,9 +21,22 @@ const Register = () => {
     setUserInput(tempData);
   };
 
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoading(true);
+
+    const response: apiResponse = await registerUser({
+      userName: userInput.userName,
+      password: userInput.password,
+      role: userInput.role,
+      name: userInput.name,
+    });
+    setLoading(false);
+  };
+
   return (
     <div className="container text-center">
-      <form method="post">
+      <form method="post" onSubmit={handleSubmit}>
         <h1 className="mt-5">Register</h1>
         <div className="mt-5">
           <div className="col-sm-6 offset-sm-3 col-xs-12 mt-4">
